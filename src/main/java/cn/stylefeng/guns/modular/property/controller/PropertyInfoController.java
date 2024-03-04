2f7cd52b35970e6ab0b2a8ebec1663b540bda022
yuanye <yuanye123>
2024-03-04 10:25:48 +0800

    Changes

---
+++ src/main/java/cn/stylefeng/guns/modular/property/controller/PropertyInfoController.java
@@ -0,0 +1,27 @@
+package cn.stylefeng.guns.modular.property.controller;//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+
+
+import cn.stylefeng.roses.kernel.scanner.api.annotation.ApiResource;
+import cn.stylefeng.roses.kernel.scanner.api.annotation.GetResource;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@ApiResource(
+        name = "在线用户查看界面"
+)
+public class PropertyInfoController {
+    public PropertyInfoController() {
+    }
+
+    @GetResource(
+            name = "在线用户查看界面",
+            path = {"/view/onlineUser"}
+    )
+    public String onlineUser() {
+        return "/modular/system/onlineUser/online_user.html";
+    }
+}
